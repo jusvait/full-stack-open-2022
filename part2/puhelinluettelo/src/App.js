5ed22759ff715e44db6ee4c24c1cbfc9f1391551
jusvait <42100008+jusvait@users.noreply.github.com>
2022-09-14 22:06:43 +0300

    Part2 - 2.11

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const AddPerson = ({persons, onSuccess}) => {
   const [newEntry, setNewEntry] = useState({name: '', number: ''})
@@ -62,13 +63,13 @@ const Person = ({person}) => {
 
 const App = () => {
   const [filterState, setFilterState] = useState('');
+  const [persons, setPersons] = useState([])
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
 
   return (
     <div>
