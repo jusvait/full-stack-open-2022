4e55a3d605d617e10194130c297b35201c35a646
jusvait <42100008+jusvait@users.noreply.github.com>
2022-09-14 23:12:05 +0300

    Part2 - 19

--- part2/puhelinluettelo/src/App.js
+++ part2/puhelinluettelo/src/App.js
@@ -1,36 +1,20 @@
 import { useEffect, useState } from 'react'
 import peopleService from './services/people'
 
-const AddPerson = ({persons, onSuccess}) => {
-  const [newEntry, setNewEntry] = useState({name: '', number: ''})
+import './App.css'
 
-  const initialEntryValues = {
-    name: '',
-    number: '',
-  }
-
+const AddPerson = ({newEntry, onSubmit, onChange}) => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    
-    if (persons.some((val) => val.name === newEntry.name)) {
-      alert(`${newEntry.name} is already added to phonebook`); return;
-    }
-
-    peopleService
-      .create(newEntry)
-      .then(newPerson => {
-        onSuccess(persons.concat(newPerson))
-      })
-
-    setNewEntry(initialEntryValues)
+    onSubmit();
   }
 
   const handleNameChange = (event) => {
-    setNewEntry({...newEntry, name: event.target.value})
+    onChange({...newEntry, name: event.target.value})
   }
 
   const handleNumberChange = (event) => {
-    setNewEntry({...newEntry, number: event.target.value})
+    onChange({...newEntry, number: event.target.value})
   }
   return (
     <><form onSubmit={handleSubmit}>
@@ -65,16 +49,35 @@ const People = ({persons, onRemove}) => {
 const Person = ({person, onRemove}) => {
   const remove = () => {
     if (window.confirm(`Do you really want to delete ${person.name}?`)) {
-      onRemove(person.id)
+      onRemove(person)
     }
   }
 
   return (<><p>{person.name} {person.number}</p><button onClick={() => remove()}>Delete</button></>)
 }
 
+const Notification = ({message}) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
+  const initialEntryValues = {
+    name: '',
+    number: '',
+  }
+
   const [filterState, setFilterState] = useState('');
   const [persons, setPersons] = useState([])
+  const [newEntry, setNewEntry] = useState(initialEntryValues)
+  const [errorState, setErrorState] = useState(null)
 
   useEffect(() => {
     peopleService
@@ -84,20 +87,45 @@ const App = () => {
       })
   }, [])
 
-  const onPersonRemove = (id) => {
+  const showMessage = (msg, dur) => {
+    setErrorState(msg)
+        setTimeout(() => {
+          setErrorState(null)
+        }, dur * 1000)
+  }
+
+  const onPersonAdd = () => {
+    if (persons.some((val) => val.name === newEntry.name)) {
+      alert(`${newEntry.name} is already added to phonebook`);
+      return;
+    }
+
     peopleService
-      .remove(id)
+      .create(newEntry)
+      .then(newPerson => {
+        setPersons(persons.concat(newPerson))
+        showMessage(`Added ${newEntry.name}`, 4)
+      })
+
+    setNewEntry(initialEntryValues)
+  }
+
+  const onPersonRemove = (person) => {
+    peopleService
+      .remove(person.id)
       .then(removed => {
-        setPersons(persons.filter(p => p.id !== id))
+        setPersons(persons.filter(p => p.id !== person.id))
+        showMessage(`Removed ${person.name}`, 4)
       })
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorState} />
       <NameFilter filterValue={filterState} onFilterChange={setFilterState}/>
       <h2>Add a new</h2>
-      <AddPerson persons={persons} onSuccess={setPersons}/>
+      <AddPerson newEntry={newEntry} onChange={setNewEntry} onSubmit={onPersonAdd}/>
       <h2>Number</h2>
       <People persons={persons.filter(v => v.name.toLocaleLowerCase().startsWith(filterState))} onRemove={onPersonRemove}/>
     </div>
